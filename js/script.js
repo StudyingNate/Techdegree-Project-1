8cd785ca74c9037e0059e8565a8d68e5c2ff2bd8
Nate <studyingnate@gmail.com>
2022-04-09 18:03:04 -0700

    First Draft
    Testing GitHub,
    
    Adding draft code for project 1.

---
+++ js/script.js
@@ -0,0 +1,72 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+var quotes = [
+  {
+    quote: 'Macintosh',
+    source: 'Red'
+    citation:
+    year:
+    tags:
+  },
+  {
+    quote: 'Macintosh',
+    source: 'Red'
+    citation:
+    year:
+    tags:
+  },
+  {
+    quote: 'Macintosh',
+    source: 'Red'
+    citation:
+    year:
+    tags:
+  },
+  {
+    quote: 'Macintosh',
+    source: 'Red'
+    citation:
+    year:
+    tags:
+  },
+  {
+    quote: 'Macintosh',
+    source: 'Red'
+    citation:
+    year:
+    tags:
+  }
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+const getRandomQuote = () => {
+
+  return qoute[i]
+}
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
